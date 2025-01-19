e754937ee603231fdee2b18a2f14cd6736361193
220daniel <a220daniel@yahoo.com.hk>
2025-01-19 15:57:40 +0800

    ex_02

---
+++ 02.js
@@ -0,0 +1,29 @@
+let a=4
+let b=6
+let c=9
+let d=10
+let e=15
+
+console.log(a<b && b<c && c<d && d<e)
+
+console.log('-------')
+
+let hasCheesePromotion = true
+let buyMilk = 'buy milk'
+let buyCheese = 'buy Cheese'
+if (hasCheesePromotion == true)  {
+    console.log('Case01 : ',buyMilk, 'and' ,buyCheese)
+} else {
+    console.log('Case01 : ',buyMilk)
+}
+
+if (hasCheesePromotion == true)  {
+    console.log('Case02 : ',buyCheese)
+} else {
+    console.log('Case02 : ',buyMilk)
+}
+
+
+
+
+
